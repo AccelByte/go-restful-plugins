cc5a312a825c12514219cf10fb03a14947a458a9
Anggoro Dewanto <anggorodewanto@gmail.com>
2018-09-15 15:07:06 +0700

    Add Common Log Format logger

---
+++ pkg/logger/common/README.md
@@ -0,0 +1,22 @@
+# Common Log Format Logger
+
+This package enables logging using Common Log Format in go-restful apps.
+
+## Usage
+
+### Log all endpoints
+
+```go
+ws := new(restful.WebService)
+ws.Filter(common.Log)
+```
+
+### Log specific endpoint
+
+```go
+ws := new(restful.WebService)
+ws.Route(ws.GET("/user/{id}").
+    Filter(common.Log).
+    To(func(request *restful.Request, response *restful.Response) {
+}))
+```
---
+++ pkg/logger/common/common.go
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2018 AccelByte Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package common
+
+import (
+	"github.com/emicklei/go-restful"
+	"github.com/sirupsen/logrus"
+	"strings"
+	"time"
+)
+
+// Log is a filter that will log incoming request with Common Log Format
+func Log(req *restful.Request, resp *restful.Response, chain *restful.FilterChain) {
+	username := "-"
+	if req.Request.URL.User != nil {
+		if name := req.Request.URL.User.Username(); name != "" {
+			username = name
+		}
+	}
+	chain.ProcessFilter(req, resp)
+	logrus.Infof("%s - %s [%s] \"%s %s %s\" %d %d",
+		strings.Split(req.Request.RemoteAddr, ":")[0],
+		username,
+		time.Now().Format("02/Jan/2006:15:04:05 -0700"),
+		req.Request.Method,
+		req.Request.URL.RequestURI(),
+		req.Request.Proto,
+		resp.StatusCode(),
+		resp.ContentLength(),
+	)
+}
