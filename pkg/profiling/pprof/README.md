890d2d1c3614baf84e2847c71b85f143aab4c36d
Anggoro Dewanto <anggorodewanto@gmail.com>
2018-10-15 08:56:59 +0700

    add pprof route

---
+++ pkg/profiling/pprof/README.md
@@ -0,0 +1,17 @@
+# pprof Routes
+
+This package adds pprof profiling routes to go-restful apps.
+
+## Usage
+
+### Importing
+
+```go
+import "github.com/AccelByte/go-restful-plugins/pkg/profiling/pprof"
+```
+
+### Add pprof routes
+
+```go
+pprof.Route("/basepath")
+```
---
+++ pkg/profiling/pprof/pprof.go
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2018 AccelByte Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package pprof
+
+import (
+	"net/http"
+	netProf "net/http/pprof"
+)
+
+// Route registers pprof routes based on the basepath
+func Route(basePath string) {
+	http.HandleFunc(basePath+"/debug/pprof/", netProf.Index)
+	http.HandleFunc(basePath+"/debug/pprof/cmdline", netProf.Cmdline)
+	http.HandleFunc(basePath+"/debug/pprof/profile", netProf.Profile)
+	http.HandleFunc(basePath+"/debug/pprof/symbol", netProf.Symbol)
+	http.HandleFunc(basePath+"/debug/pprof/trace", netProf.Trace)
+}
