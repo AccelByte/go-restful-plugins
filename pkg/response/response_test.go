5dcce8348407a28d0142293cbd41f2ec8dd7dc09
Adiel Julio <adiel@accelbyte.net>
2019-12-12 11:00:37 +0700

    CR-607 Add WriteErrorWithEventID in response pkg

--- pkg/response/response_test.go
+++ pkg/response/response_test.go
@@ -143,3 +143,85 @@ func TestWriteErrorInternalServerError(t *testing.T) {
 	assert.Equal(t, http.StatusInternalServerError, resp.Code, "response status code should be %v", http.StatusOK)
 	assert.Equal(t, expected, responseTest, "response body must be %+v", expected)
 }
+
+// nolint: dupl // most part of the test is identical
+func TestWriteErrorWithEventIDWarning(t *testing.T) {
+	ws := new(restful.WebService)
+	ws.Filter(event.Log("test", "go-restful-plugins", util.ExtractDefault))
+
+	ws.Route(
+		ws.GET("/namespace/{namespace}/user/{id}").
+			Param(restful.PathParameter("namespace", "namespace")).
+			Param(restful.PathParameter("id", "user ID")).
+			To(func(request *restful.Request, response *restful.Response) {
+				WriteErrorWithEventID(request, response, http.StatusNotFound, 0, 40, errors.New("123"),
+					&Error{
+						ErrorCode:    30,
+						ErrorMessage: "abc",
+						ErrorLogMsg:  "cba",
+					})
+			}))
+
+	container := restful.NewContainer()
+	container.Add(ws)
+
+	req := httptest.NewRequest(http.MethodGet, "/namespace/abc/user/def", nil)
+	req.Header.Set("X-Forwarded-For", "8.8.8.8")
+
+	resp := httptest.NewRecorder()
+	container.ServeHTTP(resp, req)
+
+	decoder := json.NewDecoder(resp.Body)
+	var responseTest Error
+	_ = decoder.Decode(&responseTest)
+
+	expected := Error{
+		ErrorCode:    30,
+		ErrorMessage: "abc",
+		ErrorLogMsg:  "",
+	}
+
+	assert.Equal(t, http.StatusNotFound, resp.Code, "response status code should be %v", http.StatusOK)
+	assert.Equal(t, expected, responseTest, "response body must be %+v", expected)
+}
+
+// nolint: dupl // most part of the test is identical
+func TestWriteErrorWithEventIDInternalServerError(t *testing.T) {
+	ws := new(restful.WebService)
+	ws.Filter(event.Log("test", "go-restful-plugins", util.ExtractDefault))
+
+	ws.Route(
+		ws.GET("/namespace/{namespace}/user/{id}").
+			Param(restful.PathParameter("namespace", "namespace")).
+			Param(restful.PathParameter("id", "user ID")).
+			To(func(request *restful.Request, response *restful.Response) {
+				WriteErrorWithEventID(request, response, http.StatusInternalServerError, 0, 41, errors.New("323"),
+					&Error{
+						ErrorCode:    31,
+						ErrorMessage: "abc",
+						ErrorLogMsg:  "cba",
+					})
+			}))
+
+	container := restful.NewContainer()
+	container.Add(ws)
+
+	req := httptest.NewRequest(http.MethodGet, "/namespace/abc/user/def", nil)
+	req.Header.Set("X-Forwarded-For", "8.8.8.8")
+
+	resp := httptest.NewRecorder()
+	container.ServeHTTP(resp, req)
+
+	decoder := json.NewDecoder(resp.Body)
+	var responseTest Error
+	_ = decoder.Decode(&responseTest)
+
+	expected := Error{
+		ErrorCode:    31,
+		ErrorMessage: "abc",
+		ErrorLogMsg:  "",
+	}
+
+	assert.Equal(t, http.StatusInternalServerError, resp.Code, "response status code should be %v", http.StatusOK)
+	assert.Equal(t, expected, responseTest, "response body must be %+v", expected)
+}
