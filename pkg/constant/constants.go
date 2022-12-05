eac5cbfc6699f66fa5e84a27491afb3cab4fa849
arya-ab <83328142+arya-ab@users.noreply.github.com>
2022-12-05 12:34:21 +0700

    chore: add dev stack tracer for insufficient error details (#63)
    * chore: add dev stack tracer for insufficient error details

--- pkg/constant/constants.go
+++ pkg/constant/constants.go
@@ -15,7 +15,15 @@
 package constant
 
 const (
-	ContentType = "Content-Type"
-	Referer     = "Referer"
-	UserAgent   = "User-Agent"
+	ContentType      = "Content-Type"
+	Referer          = "Referer"
+	UserAgent        = "User-Agent"
+	DefaultRealmLive = "prod,live,demo,trial"
+)
+
+const (
+	PermissionRead   = "READ"
+	PermissionCreate = "CREATE"
+	PermissionUpdate = "UPDATE"
+	PermissionDelete = "DELETE"
 )
